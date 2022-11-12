42b197a2bc511861333d819b1d9626f0ea78866b
butrusm <74104667+butrusm@users.noreply.github.com>
2022-11-12 15:36:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 HEIGHT = 768
 WIDTH = 1024
 
@@ -18,13 +17,6 @@ lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 mainBox = tk.Label(lower_frame, font = ('Courier', 20),bg = '#DEE4DF')
 mainBox.place(relwidth=2, relheight=2)
 
-# updateButtonFrame = tk.Frame(root, bg=background_green, bd=5)
-# updateButtonFrame.place(relx=1.1, rely=0.05, relwidth=1, relheight=0.1, anchor='n')
-
-# updateButton = tk.Button(updateButtonFrame, text= "Get Voting Results", bg='#5390D9', fg='black', font=('Courier',20), command=lambda : updateVote(voterDatabase))
-# updateButton.place(relheight=1, relwidth=0.3)
-
-
 inputBoxDestinationCountry = tk.Frame(root, bg='#DEE4DF', bd=5)
 inputBoxDestinationCountry.place(relx=0.52, rely = 0.55, relwidth=0.4, relheight=0.05, anchor='n')
 
@@ -34,15 +26,15 @@ destinationCountry.place(relwidth=1, relheight=1)
 provinceLabelBox = tk.Frame(root, bg='#DEE4DF', bd=5)
 provinceLabelBox.place(relx=0.25, rely = 0.55, relwidth=0.15, relheight=0.05, anchor='n')
 
-votePartyLabel = tk.Label(provinceLabelBox, font = ('Courier', 12), bg = '#DEE4DF', fg = '#333')
-votePartyLabel.place(relwidth = 1, relx = 0)
-votePartyLabel['text'] = "Country:"
+provinceLabel = tk.Label(provinceLabelBox, font = ('Courier', 12), bg = '#DEE4DF', fg = '#333')
+provinceLabel.place(relwidth = 1, relx = 0)
+provinceLabel['text'] = "Country:"
 
 inputBoxProvinceState = tk.Frame(root, bg='#DEE4DF', bd=5)
 inputBoxProvinceState.place(relx=0.52, rely = 0.50, relwidth=0.4, relheight=0.05, anchor='n')
 
-destinationProvinceState = tk.Entry(inputBoxProvinceState, font=('Courier',40),bg='#9ECCD7')
-destinationProvinceState.place(relwidth=1, relheight=1)
+destinationProvince_State = tk.Entry(inputBoxProvinceState, font=('Courier',40),bg='#9ECCD7')
+destinationProvince_State.place(relwidth=1, relheight=1)
 
 destinationProvinceState = tk.Frame(root, bg='#DEE4DF', bd=5)
 destinationProvinceState.place(relx=0.25, rely = 0.50, relwidth=0.15, relheight=0.05, anchor='n')
@@ -64,19 +56,6 @@ destenationCityLabel = tk.Label(destenationCityBox, font = ('Courier', 12), bg =
 destenationCityLabel.place(relwidth = 1, relx = 0)
 destenationCityLabel['text'] = "City:"
 
-# destinationCountry = tk.Frame(root, bg=background_green, bd=5)
-# destenationCountry.place(relx=0.30, rely = 0.50, relwidth=0.15, relheight=0.06, anchor='n')
-
-# destinationCountryLabel = tk.Label(currentCountry, font = ('Courier', 10), bg = background_green)
-# destinationCountryLabel.place(relwidth = 1, relx = 0)
-# destinationCountryLabel['text'] = "Destination Country:"
-
-# usernameLabelBox = tk.Frame(root, bg=background_green, bd=5)
-# usernameLabelBox.place(relx=0.25, rely = 0.06, relwidth=0.1, relheight=0.05, anchor='n')
-
-# usernameLabel = tk.Label(usernameLabelBox, font = ('Courier', 10), bg = background_green)
-# usernameLabel.place(relwidth = 1, relx = 0)
-# usernameLabel['text'] = "Closest City:"
 
 titleFrame = tk.Frame(root, bg='#DEE4DF', bd=5)
 titleFrame.place(relx=0.5, rely = 0.15, relwidth=0.30, relheight=0.05, anchor='n')
@@ -85,24 +64,17 @@ titleLabel = tk.Label(titleFrame, font = ('Courier', 16), bg = '#DEE4DF', fg = '
 titleLabel.place(relwidth = 1, relx = 0)
 titleLabel['text'] = "Where would you like to go?"
 
-# inputButtonFrame = tk.Frame(root, bg=background_green, bd=5)
-# inputButtonFrame.place(relx=0.3, rely=0.175, relwidth=0.35, relheight=0.05, anchor='n')
 
-# loginButton = tk.Button(inputButtonFrame, text= "Authenticate and Vote!", bg='#5390D9', fg='black', font=('Courier',20), command=lambda : authenticateAndVote(username.get(), voteParty.get(), voterDatabase))
-# loginButton.place(relheight=1, relwidth=1)
+def enter(a,z,x):
+    print('Enter')
+    print(a,z,x)
+    
+updateButtonFrame = tk.Frame(root, bg='#333', bd=5)
+updateButtonFrame.place(relx=0.5, rely=0.70, relwidth=0.2, relheight=0.1, anchor='n')
 
-# import everything from tkinter module
-
-
+updateButton = tk.Button(updateButtonFrame, text= "Enter", bg='#DEE4DF', fg='black', font=('Courier',20), command=lambda: enter(destinationCountry.get(), destenationCity.get(), destinationProvince_State.get()))
+updateButton.place(relheight=1, relwidth=1)
 
   
-def enter(d):
-    print('Enter')
-    
-updateButtonFrame = tk.Frame(root, bg='#a5f0f0', bd=5)
-updateButtonFrame.place(relx=1, rely=0.70, relwidth=1, relheight=0.1, anchor='n')
-
-updateButton = tk.Button(updateButtonFrame, text= "Get Map Results", bg='#5390D9', fg='black', font=('Courier',20), command=lambda: enter(voteDatabase))
-updateButton.place(relheight=1, relwidth=0.3)
 
 root.mainloop()
